b3b986274fb6de48b9acfa3053ddd7ce9c17bb8d
Rubayet Hasan <hasan@l3s.de#>
2018-12-15 19:54:59 +0100

    database added

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
 
-return [
+/*return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
@@ -11,4 +11,17 @@ return [
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
+];*/
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysqldb.gwdg.de:3307;dbname=planspiel_fwos',
+    'username' => 'fwos_user',
+    'password' => '0osjV8Piy',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 60,
+    'schemaCache' => 'cache',
 ];
